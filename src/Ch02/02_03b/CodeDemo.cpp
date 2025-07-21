a7eb770c0fa2fce082ae0f60eae725656f4edc50
PRASHIN <prashin12@gmail.com>
2025-07-21 03:53:49 +0000

    variable

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,17 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdint>
+#define MY_STUDENT 50
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
+    uint32_t num;
+    std::cout << "Hi There! please enter a number" << std::endl;
+    std::cin >> num;
+    // if (num > MY_STUDENT){
+    //     std::cout << "more than 100 perople";
+    // }
+    std::cout << sizeof(num);
     std::cout << std::endl << std::endl;
     return (0);
 }
